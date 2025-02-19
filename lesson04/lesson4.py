4725a16de1ed6789ee2ef0ba56c5d203e34010e9
wewe1657 <willhoang0930@gmail.com>
2025-02-19 14:11:48 -0500

    Update lesson4.py

--- lesson04/lesson4.py
+++ lesson04/lesson4.py
@@ -1,1 +1,20 @@
 # Lesson 4
+from math import ceil
+#input
+section1 = input("Enter section 1:")
+section2 = input("Enter section 2:")
+section3 = input("Enter section 3:")
+
+#processing
+cans = len(section1)+len(section2)+len(section3)
+
+boxes = ceil(cans / 12)
+
+leftover = 12*boxes - cans
+
+totalcost = 14.95 * boxes
+
+#output
+print(f"we have {leftover} cans")
+print (f"the cost is {totalcost}")
+print(f"we need {cans} to paint the fences")
