27aff5e90e5c2b6a24405cabc59ba2e6e4fab1e8
wewe1657 <willhoang0930@gmail.com>
2025-02-19 15:00:27 -0500

    Update lesson5.py

--- lesson05/lesson5.py
+++ lesson05/lesson5.py
@@ -10,13 +10,13 @@ big_cookies = 0
 normal_cookies = 0 
 
 for current_cookie in cookies_sold:
-  #print(current_cookie)
-  if current_cookie == "c":
-  normal_cookies +- 1
-  elif current cookie == "b":
-  big_cookies +- 1
-  else:
-  print (f"{current_cookie} is not a valid item")
+#print(current_cookie)
+    if current_cookie == "c":
+        normal_cookies += 1
+    elif current_cookies == "b":
+        big_cookies += 1
+    else:
+        print (f"{current_cookie} is not a valid item")
 
 total_cookies = big_cookies+normal_cookies
 profit = (big_cookies * 1.25) + (normal_cookies*0.75)
@@ -24,3 +24,4 @@ end_day = (profit + start_money)
 
 print(f"at the end of the day we sold {total_cookies} and made a total of ${end_day}")
   
+
