0f51a6aa968ab690591a245fd0e9e2558be8b391
wewe1657 <willhoang0930@gmail.com>
2025-02-26 00:07:43 -0500

    Update lesson7.py

--- lesson07/lesson7.py
+++ lesson07/lesson7.py
@@ -6,3 +6,10 @@ from random import randrange
 diff = int(intput("enter the dc: "))
 
 player_roll = randrange(1,21)
+
+print(f"the player has rolled {player_roll}) on their D20.")
+
+if player_roll >= diff:
+  print(f"the player was sucessful as {player_roll} >+ {diff})
+else:
+  print(f"the player was not sucessful as {player_roll} < {diff})
