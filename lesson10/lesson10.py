f52a2aa989a7fe68cb71a84ddadfa6177e4b008e
wewe1657 <willhoang0930@gmail.com>
2025-02-26 21:26:49 -0500

    Update lesson10.py

--- lesson10/lesson10.py
+++ lesson10/lesson10.py
@@ -1,2 +1,13 @@
 # Lesson 10
 
+phone = input("enter last 4 digits of phone number")
+
+if phone[0] in {'8' , '9'): #if string then you can use [] to access value.
+
+  if phone[-1] in ('8' , '9'):
+
+  if phone[1] -- phone [2]:
+    print(f"the phone num with {phone} as its last 4 digs is a telemarketer")
+  else:
+    print(f"the phone num is not a telemarketer")
+  
