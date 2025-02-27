f83ea5cfb6125b78bf120c10ea735513aaf0d209
wewe1657 <willhoang0930@gmail.com>
2025-02-26 21:59:11 -0500

    Update lesson11.py

--- lesson11/lesson11.py
+++ lesson11/lesson11.py
@@ -1,1 +1,24 @@
 # Lesson 11
+
+point = input()
+
+point = point.split(' ')
+
+
+point = list(map(int, point))
+print(point)
+x, y = point
+print(f'x is {x}. ')
+print (f'y is {y}.')
+
+if x>):
+  if y> 0:
+    print(f' the point of ({x},{y}) is quadrant 1')
+  else:
+    print(f' the point of ({x},{y}) is quadrant 4')
+
+else:
+  if y> 0:
+    print (f' the point of ({x},{y}) is quadrant 2')
+  else: 
+    print (f' the point of ({x},{y}) is quadrant 3')
