a83759dbf8387cfb448b3dcf894ffaa994481f31
wewe1657 <willhoang0930@gmail.com>
2025-02-26 22:38:10 -0500

    Update lesson12.py

--- lesson12/lesson12.py
+++ lesson12/lesson12.py
@@ -1,1 +1,20 @@
 # Lesson 12
+
+month = int(input())
+day = int(input())
+
+if month == 2 and day == 18:
+  print ("special")
+else:
+  if month < 2:
+    print("before")
+  elif month > 2:
+    print ("after")
+  else:
+    if day <  18:
+      print("before")
+  else:
+      print("after")
+
+  
+  
