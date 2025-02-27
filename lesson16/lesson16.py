6a73bfc046d10f6d574c07883f301094e9b219e9
wewe1657 <willhoang0930@gmail.com>
2025-02-26 23:24:41 -0500

    Update lesson16.py

--- lesson16/lesson16.py
+++ lesson16/lesson16.py
@@ -1,1 +1,12 @@
 # Lesson 16
+
+for num int range(1,51)
+
+  if num % 3 == 0 and num % 5 ==0:
+    print("fizzbuzz")
+  elif num % 3 == 0:
+    print("fizz")
+  elif num % 5 == 0:
+    print("buzz")
+  else:
+    print(num)
