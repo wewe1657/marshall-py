ff3e2ced7ff369052dd15e31ee3771040eaf2e6e
wewe1657 <willhoang0930@gmail.com>
2025-02-26 23:27:31 -0500

    Update lesson17.py

--- lesson17/lesson17.py
+++ lesson17/lesson17.py
@@ -1,1 +1,12 @@
 # Lesson 17
+
+num = int(input("enter a value of N: "))
+
+totalProduct = 1
+multiplier = 1
+
+while multiplier <= num:
+  totalProduct = totalProduct* multiplier
+  multiplier = multiplier + 1
+
+print(f"factorial of {num} is {totalProduct})
