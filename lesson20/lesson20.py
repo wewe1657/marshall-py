c6f300f0228bbdb2818c8831fdd5a164ea8a85dd
wewe1657 <willhoang0930@gmail.com>
2025-03-20 23:51:41 -0400

    Update lesson20.py

--- lesson20/lesson20.py
+++ lesson20/lesson20.py
@@ -1,1 +1,15 @@
 # Lesson 20
+
+total_sum = 0
+
+num = 12
+factor_sum = 0 
+
+for divider in range(1,num):
+  if num % divider == 0:
+    factor_sum += divider
+
+if factor_sum == num:
+  print(f"{num} is a perfect number")
+else
+  print(f"{num} is not a perfect number")
