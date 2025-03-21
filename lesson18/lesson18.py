808064546cd82e8981a01f9343a257636140f551
wewe1657 <willhoang0930@gmail.com>
2025-03-20 23:45:28 -0400

    Update lesson18.py

--- lesson18/lesson18.py
+++ lesson18/lesson18.py
@@ -1,1 +1,14 @@
 # Lesson 18
+
+num = int(input("enter a integer: "))
+
+for divider in range(1, num+1):
+  print(f"divider varible is: {divder}.")
+
+  if num % divider == 0:
+
+    print(f"{divider} is a factor for {num}")
+
+    
+  
+          
