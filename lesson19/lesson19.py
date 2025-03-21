74d6ef42a302e719c964d7a6d6d922341a03af25
wewe1657 <willhoang0930@gmail.com>
2025-03-20 23:48:46 -0400

    Update lesson19.py

--- lesson19/lesson19.py
+++ lesson19/lesson19.py
@@ -1,1 +1,17 @@
 # Lesson 19
+
+num = int(input("enter the value of n:"))
+
+counter = 0
+
+for divder in range(1,num+1)
+
+  if num%divder ==0
+  counter +=1
+
+if counter == 2:
+
+  print(f"{num} is a prime")
+else:
+  print (f"{num} is not a prime")
+  
