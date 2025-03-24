d1dc3c91324f5a7c889426f6ee85e76cd6202f22
wewe1657 <willhoang0930@gmail.com>
2025-03-23 22:44:22 -0400

    Update lesson25.py

--- lesson25/lesson25.py
+++ lesson25/lesson25.py
@@ -1,1 +1,27 @@
 # Lesson 25
+
+num = int(input("enter a value of H:"))
+
+numCopy = num
+
+largest = 0
+
+while num % 2 == 0:
+  num//=2
+
+  largest = max(largest,2)
+
+if num != 1:
+  factor = 3
+  while num!= 1:
+    if num % factor == 0:
+      largest = max(largest, factor)
+      num //= factor
+    else:
+      factor += 2
+
+  print(f"{largest} is the largest prime factor for {num_copy}.")
+  
+      
+    
+                
