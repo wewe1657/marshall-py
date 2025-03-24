639ccee3d70dd535d08b627081029c46056c8fdb
wewe1657 <willhoang0930@gmail.com>
2025-03-23 22:30:33 -0400

    Update lesson23.py

--- lesson23/lesson23.py
+++ lesson23/lesson23.py
@@ -1,1 +1,26 @@
 # Lesson 23
+
+total_sum = 0
+counter = 0
+
+while True:
+
+  userInput = input("eneter the mark (Exit) to stop inputing")
+
+if userInput.lower().capitalize() == "Exit":
+  break:
+else:
+  mark = int(userInput)
+  if 0 <=mark<=199:
+  total_sum += mark
+  counter+= 1
+  else:
+    print("invalid input
+
+average = total_sum / counter
+
+print(f"averege is {average}")
+
+
+
+  
