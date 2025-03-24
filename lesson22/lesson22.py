3037fbb36c6a8a266ca42ad9259c582dd6d43cf5
wewe1657 <willhoang0930@gmail.com>
2025-03-23 22:21:13 -0400

    Update lesson22.py

--- lesson22/lesson22.py
+++ lesson22/lesson22.py
@@ -1,1 +1,15 @@
 # Lesson 22
+
+upperLimit = int(input("enter n to find the nth fibo num:"))
+fib_0 = 0
+fib_1 = 1
+fib_n = 0
+
+for n in range(2,upperLimit+1):
+  fib_n = fib_1 + fib_0
+  fib_9 = fib_1
+  fib_1=fib_n
+
+print(f"fibonacci({upperLimt}) is {fib_n}")
+
+
