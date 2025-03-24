609d30c8cda35ebf6453e4ed45e3accabd6080c6
wewe1657 <willhoang0930@gmail.com>
2025-03-23 22:15:53 -0400

    Update lesson21.py

--- lesson21/lesson21.py
+++ lesson21/lesson21.py
@@ -1,1 +1,19 @@
 # Lesson 21
+
+upperLimit = int(input("enter N:"))
+
+maxCount = 0 
+resultNum=0
+
+for num in range(1,upper_limit+1):
+  totalFactors = 0
+
+for divider in range(1,num+1):
+  if num%divider == 0:
+    totalFactors+=1
+    
+if totalFactors > maxCount:
+  maxCount = totalFactors
+  resultNum=num
+
+print(f"{resultNum} had the most factors with {maxCount} factors.")
