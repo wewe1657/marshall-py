9c36c517ce5374c04b23afaf16e30dfea98e350f
wewe1657 <willhoang0930@gmail.com>
2025-03-25 00:04:34 -0400

    Update lesson28.py

--- lesson28/lesson28.py
+++ lesson28/lesson28.py
@@ -1,1 +1,23 @@
 # Lesson 28
+
+def is_palindrome1(text):
+
+  return text == text[::-1]
+
+def is_palindrome2(text):
+
+  if not text:
+    return True
+  elif len(text)<4:
+    return text[0] == text[-1]
+  else:
+    midpoint=len(text)//2
+    for i in range(0,midpoint):
+      left = text[i]
+      right = text[-1*1 -1]
+      if left != right:
+        return False
+    return True
+
+print(is_palindrome1("tacocat"), is_palindrome2("tacocat"))
+print(is_palindrome1("tacodog"), is_palindrome2("tacodog"))
