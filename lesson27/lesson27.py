e49f5764bbb022b79728f12564a88abd946f3c9e
wewe1657 <willhoang0930@gmail.com>
2025-03-24 23:58:08 -0400

    Update lesson27.py

--- lesson27/lesson27.py
+++ lesson27/lesson27.py
@@ -1,1 +1,13 @@
 # Lesson 27
+
+def string_cleaner(text):
+
+  result = ""
+  for character in text:
+    if character.isalpha():
+      result += character.lower()
+  return result
+
+value = string_cleaner("hElLo WoRLD")
+
+print (f"value is {value}")
