90184ac1ab7a5bc697056732be1907988038c63e
wewe1657 <willhoang0930@gmail.com>
2025-03-25 00:15:50 -0400

    Update lesson30.py

--- lesson30/lesson30.py
+++ lesson30/lesson30.py
@@ -1,1 +1,18 @@
 # Lesson 30
+
+def create_line(num):
+
+  test=''
+  for i in range(1, num+=1):
+    if i % 2 == 0:
+      text += '0'
+    else:
+      text += '1'
+  return text
+
+def outpout_patter(num):
+  for i in range(1,num+1):
+    print(create_line(i))
+
+v = output_patter(5)
+print(v)
