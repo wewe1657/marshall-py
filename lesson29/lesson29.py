02e32ecbd656a201abd8306216d0794fbb2360c3
wewe1657 <willhoang0930@gmail.com>
2025-03-25 00:12:38 -0400

    Update lesson29.py

--- lesson29/lesson29.py
+++ lesson29/lesson29.py
@@ -1,1 +1,21 @@
 # Lesson 29
+
+def consonant_count(text, vowel=False):
+  counter = 0
+  for character in text:
+    character = character.lower()
+    if character.isalpha() and character not in {"a"."e","i","o","u"}:
+      counter += 1
+
+    if not vowel:
+      return ctr
+    else: 
+      counter = 0
+      for character in text:
+      character = character.lower()
+      if character.isalpha() and character in {"a"."e","i","o","u"}:
+      counter += 1
+      return counter
+
+print(f'the count is: {consonant_count("hello, world")}')
+print(f'the count is: {consonant_count("hello, world",True)}z')
