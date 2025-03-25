387aeae44819cecb258b0d71262c977b7d4daa02
wewe1657 <willhoang0930@gmail.com>
2025-03-24 23:53:35 -0400

    Update lesson26.py

--- lesson26/lesson26.py
+++ lesson26/lesson26.py
@@ -1,1 +1,19 @@
 # Lesson 26
+
+def is_divisible(num1, num2):
+  return num1 % num2 ==0
+
+def factor_count(number):
+  counter = 0
+  for divider in range 1, number+1):
+  if number % divider ==0:
+    counter += 1
+  return counter
+
+
+
+upper_limit = int(input("Enter N:"))
+
+for num in range (1, upperlimit+1):
+  factor_size = factor_count(num)
+  print(f"{num} has {factor_size} factors.")
