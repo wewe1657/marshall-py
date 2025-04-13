427443c5ed01d340607751343ec49c7714595cc8
wewe1657 <willhoang0930@gmail.com>
2025-04-12 23:59:17 -0400

    Update lesson31.py

--- lesson31/lesson31.py
+++ lesson31/lesson31.py
@@ -1,1 +1,15 @@
 # Lesson 31
+def anagram(word1,word2):
+  if len(word1) != len(word2):
+    return False
+  else: 
+    list_word1 = sorted(word1)
+    list_word2 = sorted(word2)
+    for i, character in enumerate(lsit_word1):
+      print(f'i:{i}')
+      print(f'chartacter:{character}')
+      print(f'list_word2[i]:{list_word2[i]}')
+      if list_word2[i] != character:
+        return False
+      input()
+    return True
