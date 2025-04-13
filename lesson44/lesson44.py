126137a3b60b631e25d1f6c81534de5b2c173b34
wewe1657 <willhoang0930@gmail.com>
2025-04-13 01:17:29 -0400

    Update lesson44.py

--- lesson44/lesson44.py
+++ lesson44/lesson44.py
@@ -1,1 +1,11 @@
 # Lesson 44
+def frequency(word):
+  clean_word = sorted(word.lower())
+  answer = {}
+  for character in clean_word:
+    if character not in answer:
+      answer[character] = 1
+    else:
+      answer[character] += 1
+  return answer
+  
