f77afc48a1a7e20fbb0c0e88fff90e662dff0676
wewe1657 <willhoang0930@gmail.com>
2025-04-13 00:34:18 -0400

    Update lesson36.py

--- lesson36/lesson36.py
+++ lesson36/lesson36.py
@@ -1,1 +1,16 @@
 # Lesson 36
+
+def factors(num):
+  result = []
+  for divider in range(1, nun+1):
+    if num % divider == 0:
+      result.append(divider)
+
+  return result
+
+def prime(num):
+  return len(factors(num)) == 2
+  
+
+              
+      
