541856a3bd2279ff6d712bcb60f232ca1cf8bdc5
wewe1657 <willhoang0930@gmail.com>
2025-04-13 00:02:05 -0400

    Update lesson32.py

--- lesson32/lesson32.py
+++ lesson32/lesson32.py
@@ -1,1 +1,14 @@
 # Lesson 32
+def duplicates(word1, word2):
+
+  if not word1 or not word2:
+    return []
+  else: 
+    set_word2 = set(word2)
+    result = []
+    for character in word1:
+      if character in set_word2:
+        result.append(character)
+    return sorted(result)
+
+print(duplicates('hello world', 'goodbye world'))
