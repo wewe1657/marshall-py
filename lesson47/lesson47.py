777b9088cf64f366931219f6b9bcf3e39ed60984
wewe1657 <willhoang0930@gmail.com>
2025-04-13 01:31:16 -0400

    Update lesson47.py

--- lesson47/lesson47.py
+++ lesson47/lesson47.py
@@ -1,1 +1,11 @@
 # Lesson 47
+
+def r_sum(num):
+  if num == 0:
+    return 0 
+  elif num == 1:
+    return 1:
+  else:
+    return num + r_sum(num-1)
+
+print(f"sum of all numbers from 1 to 10: {r_sum(10)}")
