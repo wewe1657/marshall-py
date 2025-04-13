79dabb50d2e0c1e7171f83a895dc4b807d2a98cd
wewe1657 <willhoang0930@gmail.com>
2025-04-13 01:20:32 -0400

    Update lesson45.py

--- lesson45/lesson45.py
+++ lesson45/lesson45.py
@@ -1,1 +1,17 @@
 # Lesson 45
+
+def word_length(a_list):
+  return {key: len(key) for key in a_list}
+
+def d_fib(num):
+  result = {0:0, 1:1}
+  if num in result:
+    return result
+  else:
+    for i in range(2, num+1):
+      result[i] = result[i-1] + result[i-2]
+
+    return result
+
+print(d_fib(10))
+  
