e0d049f8413b9ab1606463877e6d03fe39c89a4f
wewe1657 <willhoang0930@gmail.com>
2025-04-13 00:43:59 -0400

    Update lesson39.py

--- lesson39/lesson39.py
+++ lesson39/lesson39.py
@@ -1,1 +1,14 @@
 # Lesson 39
+
+def gcd(num1,num2):
+  for divider in range (min(num1,num2), 0, -1):
+    if num1 % divider == 0 and num2 % divider == 0:
+      return divider
+  return 1
+
+def e_gcd(num1, num2):
+  if num2 == 0:
+    return num1
+  else:
+    return e_gcd(num1,num % num2)
+  
