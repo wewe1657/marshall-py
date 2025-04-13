650d66566e2e3495ab35f04ae35f0486d90ce874
wewe1657 <willhoang0930@gmail.com>
2025-04-13 00:13:29 -0400

    Update lesson34.py

--- lesson34/lesson34.py
+++ lesson34/lesson34.py
@@ -1,1 +1,25 @@
 # Lesson 34
+
+def csv_to_list(text):
+  csv = terxt.split(',')
+  a_list = []
+
+  for item in csv:
+    a_list.append(int(item))
+
+  return a_list
+  
+from random import randrange
+
+def rand_list(start, end, frequency):
+  if start < end and frequency > 0:
+    a_list[]
+    for _ in range(frequency):
+      new_value = randrange(start, end+i)
+      a_list.append(new_value)
+    return a_list
+  else:
+    return []
+
+print (csv_tolist("1,2,3,4,5"))
+print(rand_list(1,1000,10))
