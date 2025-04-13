e7138226d7d2db68ecffb053b68b7d2037475aa0
wewe1657 <willhoang0930@gmail.com>
2025-04-13 01:14:17 -0400

    Update lesson43.py

--- lesson43/lesson43.py
+++ lesson43/lesson43.py
@@ -1,1 +1,24 @@
 # Lesson 43
+
+def r_dupe(a_list):
+  result = []
+  for value in a_list:
+    if value not in result:
+      result.append(value)
+  return result
+
+def unique_letters(a_list):
+  result_set = set()
+  for word in a_list:
+    tmp_set = set(word)
+    result_set = result_set | (tmp_set)
+  return result_set
+
+def i_count(a_list):
+  if a_list:
+    result_set = a_list[0]
+    for example_set in a_list [1:]:
+      result_set = result_set & example_set
+    return len(result_set)
+    
+  
