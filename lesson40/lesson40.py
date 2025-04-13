47b603255bedb9af41af7437236eaa99eca1b7bc
wewe1657 <willhoang0930@gmail.com>
2025-04-13 00:49:22 -0400

    Update lesson40.py

--- lesson40/lesson40.py
+++ lesson40/lesson40.py
@@ -1,1 +1,16 @@
 # Lesson 40
+
+def its_prime(num):
+  return len(factors_list(nuim)) == 2
+
+def prime_list(upper_limit):
+  primes = [2]
+  if upper_limit == 2:
+    return primes
+  else:
+    for num in range(3,upper_limit, 2):
+      if is_prime (num):
+        primes.append(num)
+    return primes
+    
+  
