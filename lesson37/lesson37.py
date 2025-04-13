eb0c8910b39f121524ab35be36163a7617ea67b6
wewe1657 <willhoang0930@gmail.com>
2025-04-13 00:37:41 -0400

    Update lesson37.py

--- lesson37/lesson37.py
+++ lesson37/lesson37.py
@@ -1,1 +1,20 @@
 # Lesson 37
+
+def str_zip(text):
+  result = ''
+  ctr = 1
+
+  for i in range(1,len(text)):
+    if text[i] == text[i-1]:
+      ctr +=
+    else:
+      result += text[i-1]
+      result += str(ctr)
+
+      ctr = 1
+    result += text9i-1] + str(ctr)
+
+    if len(result) < len(text):
+      return result
+    else: 
+      return text
