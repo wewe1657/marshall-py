6450c1bdc53869e7845b27853318a6b395c79f7d
wewe1657 <willhoang0930@gmail.com>
2025-04-13 00:07:24 -0400

    Update lesson33.py

--- lesson33/lesson33.py
+++ lesson33/lesson33.py
@@ -1,1 +1,11 @@
 # Lesson 33
+def meduan(a_list):
+  sorted_a_list = sorted(a_list)
+  middle = len(a_list) // 2
+  if len(a_list_ % 2 == 0:
+    left = sorted_a_list[middle-1]
+    right = sorted_a_list[middle]
+    return (left+right)/2
+  else: 
+    return sorted_a_list[middle]
+
